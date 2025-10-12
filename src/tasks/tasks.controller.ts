30f02830332485aab11af37aaf76ba4e0917b23d
Biwash Pradhan <biwash.pradhan@varicon.com.au>
2025-10-12 08:20:10 +0545

    chore: implemented update task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import type { Task } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -27,5 +36,10 @@ export class TasksController {
   @Delete('/:id')
   deleteTaskById(@Param('id') id: string) {
     return this.taskService.deleteTaskById(id);
+  }
+
+  @Patch('/:id')
+  updateTask(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
+    return this.taskService.updateTask(id, updateTaskDto);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -31,5 +32,25 @@ export class TasksService {
 
   deleteTaskById(id: string): Task[] {
     return this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTask(id: string, updateTaskDto: UpdateTaskDto): Task {
+    const task = this.getTaskById(id);
+    if (!task) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    const { title, description, status } = updateTaskDto;
+    if (title !== undefined) {
+      task.title = title;
+    }
+    if (description !== undefined) {
+      task.description = description;
+    }
+    if (status !== undefined) {
+      task.status = status;
+    }
+
+    return task;
   }
 }
